cb64d85d68b2381373a463b6dfabc895dbfed1a4
James Dindi <dindijjames@gmail.com>
2017-12-26 10:53:50 +0300

    [part 2] Some blog app

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :comments
     validates :title, presence: true, length: { maximum: 50, minimum: 10 }
     validates :post, presence: true
 end
