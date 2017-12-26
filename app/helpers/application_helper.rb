c1652f7ba3dffe10855599c5d0b1a7dd3d66d772
James Dindi <dindijjames@gmail.com>
2017-12-26 12:32:34 +0300

    Work on tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def get_season()
+
+        time = Time.new
+        if(time.month >= 3) && (time.month <= 5)
+            "Yeah, it is Spring"
+        elsif(time.month > 5) && (time.month <= 8)
+            "Everyone loves Summer"
+        elsif(time.month > 8) && (time.month <= 10)
+            "Put on your coat, because Fall is here"
+        else
+            "Yuck, Winter"
+        end
+    end
 end
